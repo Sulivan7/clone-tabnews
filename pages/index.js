88b61b707af38496315dcc1c1817cc2b7ba5df32
Sulivan Souza <87101511+Sulivan7@users.noreply.github.com>
2023-05-05 17:43:04 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Se você só é bom em uma coisa, torne-se o melhor nela.</h1>
+    return <h1>Se você só é bom em uma coisa, torne-se o melhor nela. ✌ </h1>
 }
 
 export default Home;
